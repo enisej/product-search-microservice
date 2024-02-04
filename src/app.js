4c5d032ad0b0a48822dea3cd4245276dbabb69c5
enisej <iljya@gmail.com>
2024-02-05 00:35:11 +0200

    style(app.js): code style fixes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const router = require('./routes/product-router');
-const ApiError = require('./utils/api-error');
 const errorHandlingMiddleware = require('./middleware/error-handling-middleware');
 const loggingMiddleware = require('./middleware/logging-middleware');
 const requestMiddleware = require('./middleware/request-middleware');
 const responseMiddleware = require('./middleware/response-middleware');
-const {ERRORS} = require("./utils/consts");
+const { ERRORS } = require('./utils/consts');
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,7 +16,7 @@ app.use(loggingMiddleware);
 app.use(requestMiddleware);
 
 app.use('/api', router);
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.status(404).send({
     code: 404,
     message: ERRORS.NOT_FOUND,
