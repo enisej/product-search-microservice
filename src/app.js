c7cc004fe95175159d5e43ae7b9c6fef96b5e1f6
enisej <iljya@gmail.com>
2024-02-04 20:42:18 +0200

    style(workflows/lint.yml): test github workflow: Eslint

--- src/app.js
+++ src/app.js
@@ -21,6 +21,6 @@ app.use((req, res, next) => {
 
 app.use(errorHandlingMiddleware);
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
+
   console.log(`Server is running on port ${PORT}`);
 });
