10fcea9d7121fa5857cdf9ad6dc0c233744c908d
enisej <iljya@gmail.com>
2024-02-05 03:35:53 +0200

    style(codestyle): codestyle fixes

--- src/middleware/error-handling-middleware.js
+++ src/middleware/error-handling-middleware.js
@@ -1,6 +1,7 @@
 const ApiError = require('../utils/api-error');
 const { ERRORS } = require('../utils/consts');
 
+// eslint-disable-next-line consistent-return
 module.exports = function errorHandlingMiddleware(err, req, res, next) {
   if (err instanceof ApiError) {
     res.locals.error = err;
--- src/middleware/response-middleware.js
+++ src/middleware/response-middleware.js
@@ -1,19 +1,19 @@
 const xmlService = require('../services/xml-service');
 
 module.exports = function modifyResponseBody(req, res, next) {
-    const originalSend = res.send;
+  const originalSend = res.send;
 
-    res.send = async function onSend(body) {
-        const accept = req.get('Accept');
+  res.send = async function onSend(body) {
+    const accept = req.get('Accept');
 
-        if (accept && accept.includes('application/xml')) {
-            const xmlBody = await xmlService.convertJsonToXml(body);
-            res.type(accept);
-            return originalSend.call(this, xmlBody);
-        }
-        res.type('application/json')
-        return originalSend.call(this, body);
-    };
+    if (accept && accept.includes('application/xml')) {
+      const xmlBody = await xmlService.convertJsonToXml(body);
+      res.type(accept);
+      return originalSend.call(this, xmlBody);
+    }
+    res.type('application/json');
+    return originalSend.call(this, body);
+  };
 
-    return next();
+  return next();
 };
